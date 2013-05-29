b66d5a86025bbed1125f413a082383e900f992f4
Kate Hudson <katehudsondesign@gmail.com>
2013-05-29 11:32:52 -0400

    stuff

--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,16 @@ requirejs.config({
     'jquery': '../ext/js/jquery-1.9.1',
     'jquery.carousel': '../ext/js/jquery.carouFredSel-6.2.1',
     'moment': '../ext/js/moment',
+    'tabzilla': 'https://www.mozilla.org/tabzilla/media/js/tabzilla',
     'uri': '../ext/js/uri'
   },
   shim: {
-    'jquery': {
-      exports: 'jQuery'
-    },
-    'jquery.carousel': {
-      deps: ['jquery'],
-      exports: 'jQuery.fn.carouFredSel'
-    }
+    'tabzilla': ['jquery'],
+    'jquery.carousel': ['jquery']
   }
 });
 
-require(['jquery','base/carousel', 'base/webmaker', 'base/mediaGallery', 'base/privacy', 'base/ui', 'uri' ],
+require(['jquery','base/carousel', 'base/webmaker', 'base/mediaGallery', 'base/privacy', 'base/ui', 'uri', 'tabzilla' ],
   function ( $, carousel, webmaker, MediaGallery, privacy, UI, URI ) {
   'use strict';
   $(document).ready(function() {
@@ -64,19 +60,6 @@ require(['jquery','base/carousel', 'base/webmaker', 'base/mediaGallery', 'base/p
     UI.select( '#search-filter', function( val ) {
       switch ( val ) {
         case 'recent':
-          break;
-
-        // case 'recommended':
-        //   break;
-
-        case 'guide:popcorn':
-          media.search( { contentType: 'application/x-popcorn' } );
-          break;
-
-        case 'guide:thimble':
-          media.search( { contentType: 'application/x-thimble' } );
-          break;
-
         case 'title':
           media.search( { title: true } );
           break;
