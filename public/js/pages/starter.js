838e8a8469dc97297655b822335519eb00392a61
Kate Hudson <k88hudson@gmail.com>
2013-08-22 11:16:41 -0400

    [Fix bug 907368] Load more fix, custom limit for starter page

---
+++ public/js/pages/starter.js
@@ -0,0 +1,12 @@
+define(['jquery', 'base/ui', 'base/gallery'],
+  function ($, UI, Gallery) {
+  'use strict';
+
+  var gallery = new Gallery({
+    makeView: 'make-starter-make.html',
+    stickyPrefix: 'webmaker:template-',
+    defaultSearch: { tags: [ 'webmaker:template' ] },
+    limit: 20
+  });
+
+});
