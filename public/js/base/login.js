8fe3de38cb16bbf80b9ea587f560574c66b98219
Kate Hudson <k88hudson@gmail.com>
2014-02-12 19:15:49 -0500

    Fix: some UI from sso-ux

--- public/js/base/login.js
+++ public/js/base/login.js
@@ -1,44 +1,49 @@
 define(['jquery', 'webmaker-auth-client/webmaker-auth-client'], function ($, WebmakerAuthClient ) {
   'use strict';
-console.log( "wat" );
+
   var auth = new WebmakerAuthClient({
     csrfToken: $('meta[name="csrf-token"]').attr('content')
   });
 
-  var loginEl = $('#webmaker-login'),
-      logoutEl = $('#webmaker-logout');
+  var loginEl = $('#webmaker-login');
+  var logoutEl = $('#webmaker-logout');
 
-  function toggleUserData( userData ) {
-    var placeHolder = $( "#identity" ),
-        userElement = $( "div.user-name" ),
-        html = document.querySelector( "html" ),
-        lang = html && html.lang ? html.lang : "en-US";
+  function toggleUserData(userData) {
+    var placeHolder = $('#identity');
+    var userElement = $('div.user-name');
+    var lang = $('html').attr('lang') || 'en-US';
 
     if ( userData ) {
-      placeHolder.html( '<a href="{{ hostname }}/' + lang + '/account">' + userData.name + "</a>" );
-      placeHolder.before( "<img src='https://secure.gravatar.com/avatar/" +
-                          userData.hash + "?s=26&d=https%3A%2F%2Fstuff.webmaker.org%2Favatars%2Fwebmaker-avatar-44x44.png' alt='" +
-                          userData.hash + "'>" );
+      placeHolder.html('<a href="/' + lang + '/account">' + userData.username + "</a>" );
+      placeHolder.before('<img src="https://secure.gravatar.com/avatar/' +
+                          userData.emailHash + '?s=26&d=https%3A%2F%2Fstuff.webmaker.org%2Favatars%2Fwebmaker-avatar-44x44.png" alt="' +
+                          userData.emailHash + '">' );
     } else {
-      userElement.html( "<span id='identity'></span>" );
+      userElement.html('<span id="identity"></span>');
     }
   }
 
-  function showLogin(user) {
+  function onLogin(user) {
+    console.log('LOGIN', user);
+    $('#webmaker-nav').addClass('loggedin');
     toggleUserData(user);
-    loginEl.show();
-    logoutEl.hide();
-  }
-
-  function showLogout() {
-    toggleUserData();
     loginEl.hide();
     logoutEl.show();
   }
 
-  auth.on('login', showLogout);
+  function onLogout() {
+    console.log('LOGOUT');
+    $('#webmaker-nav').removeClass('loggedin');
+    toggleUserData();
+    loginEl.show();
+    logoutEl.hide();
+  }
 
-  auth.on('logout', showLogin);
+  auth.on('login', onLogin);
+  auth.on('logout', onLogout);
+  auth.on('error', function(err) {
+    console.log(err);
+  });
 
   loginEl.click(auth.login);
   logoutEl.click(auth.logout);
